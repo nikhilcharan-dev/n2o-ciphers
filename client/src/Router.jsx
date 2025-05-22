7efd4e3e840923faa9f412b41eecfdcfbc13499f
Nikhilcharan-dev <chintufru143@gmail.com>
2025-05-22 18:34:25 +0530

    Update Router.jsx

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
-    { path: '/home', element: <h1>HOME</h1> },
+    { path: '/', element: <h1>HOME</h1> },
 ]);
 
 const Router = () => {
