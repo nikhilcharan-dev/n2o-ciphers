fc1faacf1c10eac39e38836f7c5b53f3b3528108
Nikhilcharan-dev <chintufru143@gmail.com>
2025-05-26 02:56:14 +0530

    [Nikhil]: Cleared Bug

--- client/src/components/Sidebar/SideBar.jsx
+++ client/src/components/Sidebar/SideBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
-import './Sidebar.css';
+import './SideBar.css';
 
 const Sidebar = () => {
     const navigate = useNavigate();
