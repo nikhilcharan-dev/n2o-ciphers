b5018705a9665a987b6de96b0e62b9237677aa64
shaikumar0 <23A91A0560@aec.edu.in>
2025-06-14 12:50:24 +0530

    [umar] : Added Courses

--- client/src/pages/LearningPath/Courses/Courses.jsx
+++ client/src/pages/LearningPath/Courses/Courses.jsx
@@ -2,8 +2,43 @@ import React from 'react'
 
 export default function Courses() {
   return (
-    <div >
-      couses will be displayed here
-    </div>
+      <div className='coursesInfo'>
+        <div className='enrollNewCourses'>
+          <h1>Enroll New Courses</h1>
+          <ul className='newCoures'>
+            <li className="java">java</li>
+            <li className="cpp">cpp</li>
+            <li className="python">python</li>
+            <li className="sql">sql</li>
+          </ul>
+        </div>
+        <div className='coursesEnrolled'>
+          <h1>Courses Enrolled</h1>
+          <ul className='Enrolled'>
+            <li className="java">java</li>
+            <li className="cpp">cpp</li>
+            <li className="python">python</li>
+            <li className="sql">sql</li>
+          </ul>
+        </div>
+        <div className='coursesOnGoing'>
+          <h1>Courses On Going</h1>
+            <ul className='OnGoing'>
+              <li className="java">java</li>
+              <li className="cpp">cpp</li>
+              <li className="python">python</li>
+              <li className="sql">sql</li>
+            </ul>
+        </div>
+        <div className='coursesCompleted'>
+          <h1>Courses Completed</h1>
+            <ul className='Completed'>
+              <li className="java">java</li>
+              <li className="cpp">cpp</li>
+              <li className="python">python</li>
+              <li className="sql">sql</li>
+            </ul>
+        </div>
+      </div>
   )
 }
