31c5c66d0744f0f7dcff45da9d46c739aa53495a
shaikumar0 <23A91A0560@aec.edu.in>
2025-06-14 12:27:50 +0530

    [umar] : added Profile

--- client/src/pages/LearningPath/Profile/Profile.jsx
+++ client/src/pages/LearningPath/Profile/Profile.jsx
@@ -2,8 +2,45 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div >
-        this profile your profile
+    <div className='LearnerProfile'>
+      <div className='personalInfo'>
+        <h1>Personal Info</h1>
+        <div className='LearnerDetails'>
+          here
+          <ul className="personalInfoList">
+            <li>Name :</li>
+            <li>Organisation :</li>
+            <li>Email :</li>
+            <li>Phone Number :</li>
+            <li>Linkedin :</li>
+            <li>Github :</li>
+          </ul>
+          <div className='pic'>
+            <div className='ProfilePic'></div>
+            <button>uploadPhoto</button>
+          </div>
+        </div>
+      </div>
+      <div>
+      <ul className="Skills"></ul>
+          <h1>Skills</h1>
+        <ul className="skillsList">
+          <li>cpp :</li>
+          <li>java :</li>
+          <li>python :</li>
+          <li>sql :</li>
+        </ul>
+      </div>
+      <div className='EducationalInfo'>
+        <ul className="EducationalInfoList">
+          <h1>Degree</h1>
+          <li>intermediate :</li>
+          <li>class 10 :</li>
+        </ul>
+      </div>
+      <button className='editButton'>edit Profile</button>
+      
+    
     </div>
   )
 }
